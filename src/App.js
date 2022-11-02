84a306fa8bc7f6c80ae86aba080b9eb3f23864d7
sangamone <snehams.sangamone@gmail.com>
2022-11-01 23:44:13 -0700

    first

---
+++ src/App.js
@@ -0,0 +1,89 @@
+function App() {
+  return (
+    <>
+      <div className="ap-stripWht col-100 floatLft flexDisplay justifySpace alignCenter">
+            <div className="ap-stripTxt">
+                <span>IPL 2022</span>
+            </div>
+            <div className="ap-bcciLogo">
+            
+            </div>
+            <div className="ap-socialDiv">
+                <span>FOLLOW IPL</span>
+               
+                
+                
+            </div>
+        </div>
+      
+        
+        <div className="ap-main-header col-100 floatLft flexDisplay justifySpace alignCenter flxNav" id="myHeader"/>
+            <div className="ap-ipl-logo"><a href="#">
+              <img className="lazy" src="https://www.iplt20.com/assets/images/ipl-logo-new-old.png" alt=""/></a></div>
+           
+
+            <div className="site-mobile-menu site-navbar-target">
+                <div className="site-mobile-menu-header">
+                    <div className="site-mobile-menu-close mt-3">
+                    <span className="icon-close2 js-menu-toggle">×</span>
+                    </div>
+                </div>
+                <div className="site-mobile-menu-body"></div>
+            </div>
+
+            <div className="site-navbar-wrap">
+                <div className="site-navbar site-navbar-target">
+                    <div className="row align-items-center">
+                        <nav className="site-navigation textRight" role="navigation">
+                            <div className="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#" className="site-menu-toggle js-menu-toggle text-white"><span className="icon-menu h3"><i className="fa fa-bars"></i></span></a></div>
+                            <ul className="site-menu main-menu js-clone-nav d-none d-lg-block textCenter">
+                            
+                                  <li className="  has-children">
+                                   <a href="#">Matches</a>
+                                   </li>
+                                 <li className="  has-children">
+
+                                    <a href="#">Videos</a>
+                                                                      
+                                                                       </li>
+                                  <li className="  has-children">
+                                                                
+                                    <a href="#">Stats</a>
+                                     </li>
+									 
+                                   <li className="  has-children">
+                                                               
+                                    <a href="#">Points Table</a>
+                                    </li>
+                                   <li className="  has-children">
+
+                                    <a href="#">Fantasy</a>
+                                    </li>
+                                    <li className="  has-children">
+
+                                    <a href="#">Teams</a>
+
+                                     </li>
+                                   <li className="  has-children">
+                                                                
+                                    <a href="#">News</a>
+                                                                       
+                                   </li>
+                                 <li className="  has-children">
+                                                                
+                              
+                                    <a href="#">More</a>
+                                                                      
+                                                                    </li>
+                                                               </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+</>
+    
+     );
+}
+
+export default App;
+
